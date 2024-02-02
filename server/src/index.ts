cf1453fedcc96b12ac89b1dbc02c442a8fb23a01
WiseyXD <aryan.s.nag@gmail.com>
2024-02-02 22:22:57 +0530

    fix : Switched from bun to node

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,36 @@
+require("dotenv").config();
 import express, { Request, Response } from "express";
-import cors from "cors";
 import bodyParser from "body-parser";
+import cors from "cors";
+import Errors from "undici-types/errors";
+
 const app = express();
 const PORT = process.env.LOCALPORT;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/", (req: Request<{}, {}, { name: string }, {}>, res: Response) => {
-    const { name } = req.body;
-    res.status(200).json({
-        name: "Aryan",
-    });
-});
+app.get(
+    "/",
+    (
+        req: Request<
+            { params: string },
+            {},
+            { body: string },
+            { query: string }
+        >,
+        res: Response
+    ) => {
+        try {
+            const name = "Aryan";
+            res.status(200).json({ name });
+        } catch (error: any) {
+            const message = error.message;
+            res.status(500).json({ message });
+        }
+    }
+);
 
 app.listen(PORT, () => {
-    console.log("Server is listening at " + PORT);
+    console.log("Server is Listening at port " + PORT);
 });

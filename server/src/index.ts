e16a327f43b5a8bcbf7fdd34f78515e75ed8ba28
WiseyXD <aryan.s.nag@gmail.com>
2024-02-08 21:56:47 +0530

    fix : PORT fallback added

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 
 const app = express();
-const PORT = process.env.LOCALPORT;
+const PORT = process.env.LOCALPORT || 8000;
 
 app.use(cors());
 app.use(bodyParser.json());
